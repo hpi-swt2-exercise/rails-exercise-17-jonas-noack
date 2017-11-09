3084283a2fd31e2ea0d7ed8027adecb80bd9db22
Jonas Noack <jonas.noack@student.hpi.de>
2017-11-09 20:42:02 +0100

    Added failing test for #10.

--- spec/features/author/author_index_spec.rb
+++ spec/features/author/author_index_spec.rb
@@ -6,11 +6,12 @@ describe "When Index is shown", type: :feature do
         visit authors_path
     end
 
-    it "should show author alan" do
+    it "should show author alan and homepage" do
         alan = FactoryGirl.create :author
         visit authors_path
      
-        expect(page).to have_text("Alan")
+        expect(page).to have_text(alan.name)
+        expect(page).to have_text(alan.homepage)
     end
     
 end
