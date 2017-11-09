dcc065f894b37d34799cfd9dda09a3e17fc6f028
Jonas Noack <jonas.noack@student.hpi.de>
2017-11-09 22:21:18 +0100

    Added failing test for #17

--- app/models/author.rb
+++ app/models/author.rb
@@ -2,6 +2,10 @@ class Author < ActiveRecord::Base
     validates :last_name, presence: true
 
     def name
-       first_name + " " + last_name
+        if last_name?
+            first_name + " " + last_name
+        else
+            first_name
+       end 
     end
 end
