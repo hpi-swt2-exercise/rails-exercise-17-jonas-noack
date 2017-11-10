7e112046976ec15ac160f3280839a288a4dde1d7
Jonas Noack <jonas.noack@student.hpi.de>
2017-11-10 09:38:16 +0100

    Added Validation for Paper year has to be integer. Closes #32.

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -2,4 +2,6 @@ class Paper < ActiveRecord::Base
     validates :title, presence: true
     validates :venue, presence: true
     validates :year, presence: true
+
+    validates :year, numericality: { only_integer: true }
 end
