0300db961ba78cc31fa5c21abc9ad85e2b733831
Jonas Noack <jonas.noack@student.hpi.de>
2017-11-10 09:27:24 +0100

    Added Validation for Paper. Closes #30 #29.

--- spec/features/paper/new_paper_spec.rb
+++ spec/features/paper/new_paper_spec.rb
@@ -4,5 +4,14 @@ describe "New author page", type: :feature do
     it "should render without error" do
         visit new_paper_path
     end
+
+    it 'should show validation errors' do
+        visit new_paper_path
+        fill_in 'paper_title', with: 'Miachael'
+        fill_in 'paper_year', with: '2012'
+        find('input[type="submit"]').click
+    
+        expect(page).to have_content("Venue can't be blank")
+    end
     
 end
