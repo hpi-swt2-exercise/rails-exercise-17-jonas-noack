5110e76fdc612fd3e4da5448c929db4367173fff
Jonas Noack <jonas.noack@student.hpi.de>
2017-11-10 01:02:43 +0100

    rafactored validation for title

--- app/controllers/papers_controller.rb
+++ app/controllers/papers_controller.rb
@@ -1,5 +1,49 @@
 class PapersController < ApplicationController
-  def new
-    @paper = Paper.new 
+  def index
+    @papers = Paper.all
   end
+
+  def show
+    @paper = Paper.find(params[:id])
+  end
+
+  def new
+    @paper = Paper.new
+  end
+
+  def edit
+    @paper = Paper.find(params[:id])
+  end
+
+  def create
+    @paper = Paper.new(paper_params)
+
+    if @paper.save
+      redirect_to @paper
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @paper = Paper.find(params[:id])
+
+    if @paper.update(paper_params)
+      redirect_to @paper
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @paper = Paper.find(params[:id])
+    @paper.destroy
+
+    redirect_to papers_path
+  end
+
+  private
+    def paper_params
+      params.require(:paper).permit(:title, :venue, :year)
+    end
 end
